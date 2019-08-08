8ac5b4350d1f2de590b99c3f2c7e050f71f4b168
Wijay Sharma <thewijaysharma@gmail.com>
2019-08-08 11:13:49 +0530

    Aug 8

---
+++ PickingNumbers.java
@@ -0,0 +1,74 @@
+package ProblemSolving;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+
+        Collections.sort(a);
+
+        int maxCount = 0;
+
+        for (int i=0; i<a.size(); i++) {
+
+                int count = 0;
+
+                for (int j = i; j < a.size(); j++) {
+
+                    if (a.get(j) - a.get(i) <= 1) {
+                        count++;
+                    }else {
+                        break ;
+                    }
+                }
+
+                if(count > maxCount){
+                    maxCount  = count;
+                }
+
+        }
+
+        return maxCount;
+    }
+
+}
+
+public class PickingNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("OutputFile.txt"));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
